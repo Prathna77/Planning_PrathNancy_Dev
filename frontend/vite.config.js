b7ce2d934ad4e917f6a7d82779932cdc5563c1aa
Prathna <richardchum77@gmail.com>
2026-01-14 10:48:30 +0100

    deploy frontend to github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "Planning_PrathNancy_Dev",
   plugins: [react()],
   server: { host: true }
 })
