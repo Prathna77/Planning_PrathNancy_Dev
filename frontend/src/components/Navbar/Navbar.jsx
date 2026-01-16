2bc8139557dc770fd37e3cc4b8c9b00715cdc457
Prathna <richardchum77@gmail.com>
2026-01-16 07:15:58 +0100

    change navbar and menu in progress ...

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,28 +6,32 @@ export default function Navbar({ year, onYearPrev, onYearNext, onOpenDrawer }) {
             <div className="navInner">
                 <div className="brand">
                     <div className="logoDot" />
-                    <div>
-                        <div className="brandTitle">Calendrier</div>
-                        <div className="brandSub">Notes • Semaine Matin/Après-midi</div>
+                    <div className="brandText">
+                        <div className="brandTop">
+                            <div className="brandTitle">Calendrier</div>
+
+                            <div className="yearControl">
+                                <button className="btn" onClick={onYearPrev} aria-label="Année précédente">◀</button>
+                                <div className="yearValue">{year}</div>
+                                <button className="btn" onClick={onYearNext} aria-label="Année suivante">▶</button>
+                            </div>
+                        </div>
+
+                        {/* <div className="brandSub">Notes • Semaine Matin/Après-midi</div> */}
                     </div>
                 </div>
 
                 <div className="navActions">
-                    <div className="yearControl">
-                        <button className="btn" onClick={onYearPrev} aria-label="Année précédente">◀</button>
-                        <div className="yearValue">{year}</div>
-                        <button className="btn" onClick={onYearNext} aria-label="Année suivante">▶</button>
-                    </div>
-
                     {/* <button className="burgerBtn" onClick={onOpenDrawer} aria-label="Ouvrir le menu">
                         <span />
                         <span />
                         <span />
                     </button> */}
                     <button className="settingBtn" onClick={onOpenDrawer} aria-label="Ouvrir le menu">
-                        <i class="bi bi-gear-fill"></i>
+                        <i className="bi bi-gear-fill"></i>
                     </button>
                 </div>
+
             </div>
         </div>
     );
